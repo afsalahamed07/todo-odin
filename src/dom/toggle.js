1e3aaa5005eee5bc753e62a0d805c9f56c02e439
agrippa <afsalahamed07@gmail.com>
2024-07-17 13:26:09 +0530

    style refactoring to toggle

--- src/dom/toggle.js
+++ src/dom/toggle.js
@@ -1,6 +1,6 @@
 function createToggle() {
   const label = document.createElement("label");
-  label.classList.add("inline-flex", "items-center", "cursor-pointer");
+  label.classList.add("toggle");
 
   const input = document.createElement("input");
   input.setAttribute("type", "checkbox");
@@ -10,38 +10,18 @@ function createToggle() {
 
   const div = document.createElement("div");
   div.classList.add(
-    "relative",
-    "w-11",
-    "h-6",
-    "bg-gray-200",
-    "peer-focus:outline-none",
-    "rounded-full",
     "peer",
-    "dark:bg-gray-700",
     "peer-checked:after:translate-x-full",
     "rtl:peer-checked:after:-translate-x-full",
     "peer-checked:after:border-white",
-    "after:content-['']",
-    "after:absolute",
-    "after:top-[2px]",
-    "after:start-[2px]",
-    "after:bg-white",
-    "after:border-gray-300",
-    "after:border",
-    "after:rounded-full",
-    "after:h-5",
-    "after:w-5",
-    "after:transition-all",
-    "dark:border-gray-600",
     "peer-checked:bg-blue-600",
   );
   label.appendChild(div);
 
-
   const getToggle = () => label;
   const getToggleInput = () => input;
 
-  return {getToggle, getToggleInput};
+  return { getToggle, getToggleInput };
 }
 
 export { createToggle };

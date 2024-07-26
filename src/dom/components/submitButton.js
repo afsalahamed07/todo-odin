476a25300700155ea11c0fdb10b6391609c3a290
agrippa <afsalahamed07@gmail.com>
2024-07-26 17:03:24 +0530

    submit button coponent from button component with its styling

--- src/dom/components/submitButton.js
+++ src/dom/components/submitButton.js
@@ -1,15 +1,10 @@
 import { createButton } from "./button";
 
 function createSubmitButton() {
-  const onClick = () => {
-    const form = document.getElementById("form");
-    form.submit();
-  }
+  const submitButton = createButton("", "Submit");
+  submitButton.setClass(["submit-btn"]);
 
-  const submitButton = createButton("Submit", "Submit", onClick);
-
-  const getButton = () => submitButton;
-
-
-  return Object.assign(submitButton, {getButton});
+  return Object.assign(submitButton, {});
 }
+
+export { createSubmitButton };
